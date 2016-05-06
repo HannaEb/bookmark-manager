f290d71ecf9a395839793242a650a4e2aaf15c0f
Hanna <hannaeberhoefer@yahoo.com>
2016-05-06 12:38:19 +0100

    Added password authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,21 @@ class User
   validates_format_of :email, as: :email_address
 
   property :id, Serial
-  property :email, String, required: true
+  property :email, String, required: true, unique: true
   property :password_digest, Text
 
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.authenticate(email, password)
+    user = first(email: email)
+    if user && BCrypt::Password.new(user.password_digest) == password
+      user
+    else
+      nil
+    end
   end
 
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -6,10 +6,10 @@ feature 'User signs up' do
     expect(User.first.email).to eq('stuff@what.com')
   end
 
-  scenario "with a passwrd that does not match" do
+  scenario "with a password that does not match" do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq('/users')
-    expect(page).to have_content "Password and confirmation password do not match"
+    expect(page).to have_content "Password does not match the confirmation"
     end
 
     def sign_up(email: 'stuff@what.com',
@@ -24,11 +24,47 @@ feature 'User signs up' do
   end
 
   scenario "I can't sign up without an email address" do
-    expect { sign_up(email: nil) }.to change(User, :count).by(0)
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email must not be blank')
   end
 
   scenario "I can't sign up with an invalid email address" do
     expect { sign_up(email: "invalid@email") }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email has an invalid format')
   end
+
+  scenario 'I cannot sign up with an existing email'  do
+    sign_up
+    expect {sign_up}.to_not change(User, :count)
+    expect(page).to have_content('Email is already taken')
+  end
+
+end
+
+feature 'User sign in' do
+
+  let(:user) do
+    User.create(email: 'user@example.com',
+                password: 'secret1234',
+                password_confirmation: 'secret1234')
+  end
+
+  scenario 'with correct credentials' do
+    sign_in(email: user.email, password: user.password)
+    expect(page).to have_content "Welcome #{user.email}"
+  end
+
+  def sign_in(email:, password:)
+    visit '/sessions/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    click_button 'Sign in'
+  end
+
+    it 'does not authenticate when given an incorrect password' do
+      expect(User.authenticate(user.email, 'wrong_stupid_password')).to be_nil
+    end
 
 end
